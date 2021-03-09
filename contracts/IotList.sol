b42cda6ddba6c6015ea01c71f6799214b9168814
Wordh Ul Hasan <wordh.u.hasan@ieee.org>
2021-03-08 20:58:22 -0600

    Project Setup

---
+++ contracts/IotList.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract IotList {
+    uint public transactionCount = 0;
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const IotList = artifacts.require("IotList");
+
+module.exports = function(deployer) {
+  deployer.deploy(IotList);
+};
